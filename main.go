48b92d910ceccd9fe5bd46511bf956bdb7189373
walter.chen <24400338+xingxingso@users.noreply.github.com>
2023-05-29 10:17:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/xingxingso/gin"
@@ -9,10 +10,16 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping",
+		func(c *gin.Context) {
+			fmt.Println("is coming")
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		},
+		func(c *gin.Context) {
+			fmt.Println("is done")
+		},
+	)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
